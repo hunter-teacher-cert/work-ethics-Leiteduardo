82672805e35651a20bdb0cf0492781983fc150e4
Leiteduardo <85713750+Leiteduardo@users.noreply.github.com>
2021-11-04 22:13:59 -0400

    Create regexp.py

---
+++ regexp.py
@@ -0,0 +1,18 @@
+
+#This will search for the name of famous soccer players
+import re
+
+def find_name(line):
+    pattern = r"(M|A|K)\.?( [A-Z][a-z]*)( [A-Z][A-Za-z]*)*"
+    result = re.findall(pattern,line)
+
+    pattern = r"Roberto Carlos"
+    result = result + re.findall(pattern,line)
+    return result
+
+f = open("namefile.dat")
+for line in f.readlines():
+    #print(line)
+    result = find_name(line)
+    if (len(result)>0):
+        print(result)
